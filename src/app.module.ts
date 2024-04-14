543d44099164f47c2cceec0899e12c99a0252506
tuannm99 <tuannm@foobla.com>
2024-04-14 12:56:49 +0700

    fix - config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
+import typeorm from './config/typeorm';
 
 @Module({
     imports: [
-        ConfigModule.forRoot({ isGlobal: true }),
+        ConfigModule.forRoot({ isGlobal: true, load: [typeorm] }),
         TypeOrmModule.forRootAsync({
             inject: [ConfigService],
             useFactory: (configService: ConfigService) => configService.get('typeorm'),
