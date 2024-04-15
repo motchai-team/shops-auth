b95ea7f686d805205586adb5aeb7bfea9292ca32
tuannm99 <tuannm@foobla.com>
2024-04-15 22:01:59 +0700

    logger

--- src/modules/auth/strategy/google.strategy.ts
+++ src/modules/auth/strategy/google.strategy.ts
@@ -1,10 +1,12 @@
 import { PassportStrategy } from '@nestjs/passport';
 import { ConfigService } from '@nestjs/config';
 import { Profile, Strategy } from 'passport-google-oauth20';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class GoogleOauthStrategy extends PassportStrategy(Strategy, 'google') {
+    private logger = new Logger(GoogleOauthStrategy.name);
+
     constructor(configService: ConfigService) {
         super({
             clientID: configService.get<string>('GOOGLE_CLIENT_ID'),
@@ -16,6 +18,7 @@ export class GoogleOauthStrategy extends PassportStrategy(Strategy, 'google') {
 
     async validate(_accessToken: string, _refreshToken: string, profile: Profile) {
         const { id, name, emails } = profile;
+        this.logger.log(`[Login] profile: ${profile}`);
 
         return {
             provider: 'google',
